7ce269201c9d0bea0d96e327ade8cd1303d00642
JackNel <jcneligan@gmail.com>
2015-11-24 20:25:53 -0500

    Everything is awesome

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -4,19 +4,25 @@
   (:gen-class))
 
 (defn -main [& args]
+  (println "Enter a category: Furniture, Alcohol, Toiletries, Shoes, Food, or Jewelry.")
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
         purchases (map (fn [line]
-                      (str/split line #","))
-                    purchases)
+                         (str/split line #","))
+                       purchases)
         header (first purchases)
         purchases (rest purchases)
         purchases (map (fn [line]
-                      (interleave header line))
-                    purchases)
+                         (interleave header line))
+                       purchases)
         purchases (map (fn [line]
                          (apply hash-map line))
                        purchases)
         purchases (walk/keywordize-keys purchases)
 
-        ](pprint purchases)))
+        input (read-line)
+        purchases (filter (fn [line]
+                            (= input (:category line)))
+                          purchases)]
+        (spit "filtered_purchases.edn"
+              (pr-str purchases))))
